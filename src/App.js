26de61db02ae9d394e812687f04be8fc0189037e
jbdevelop3r <justinebanogon14@gmail.com>
2023-02-15 05:02:26 +0800

    xtendly skill test

--- src/App.js
+++ src/App.js
@@ -4,25 +4,21 @@ function App() {
   return (
     <div className="main-container">
       <div className="inner-container"> 
-      
         <section className="first-section">
           <div>
             <div className="first-line">
-            <p className="floqost-title"> <span className="flo">Flo</span><span className="qast">Qast</span> </p>
-            <p className="the-parag"> The </p>
+              <p className="floqost-title"> <span className="flo">Flo</span><span className="qast">Qast</span> </p>
+              <p className="the-parag"> The </p>
             </div>
-            <p className="fastest-most"> Fastest, Most Accurate </p>
-            <p className="close-books"> Way to Close Your Books! </p>
-          </div>
+              <p className="fastest-most"> Fastest, Most Accurate </p>
+              <p className="close-books"> Way to Close Your Books! </p>
+            </div>
         </section>
-
         <section className="second-section">
-
           <div className="first-section-div">
             <p className="schedule-demo"> SCHEDULE A DEMO </p>
             <p className="learn-more"> Learn More About FloQast </p>
           </div>
-
           <div className="second-second-div">
             <div className="second-section-first-div">
               <p className="learn-how"> Learn How FloQast Can </p>
@@ -34,9 +30,7 @@ function App() {
               <button> SCHEDULE NOW </button>
           </form>
           </div>
-
         </section>
-
       </div>
     </div>
   );
